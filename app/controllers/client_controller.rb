a4b1310468704136aa8eced01f983364ab27d558
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2025-10-01 21:20:54 +0200

    code optim

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,10 +1,16 @@
 class ClientController < ApplicationController
+  before_action :init_langchain_client
+
   def ask
     if params[:value].present?
-      llm = Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"])
-      client = Langchain::Vectorsearch::Pgvector.new(index_name: "Documents", url: "postgresql://langchain_user:langchain_pwd@localhost/test_langchain_development", llm: llm)
-      response = client.ask(question: params[:value])
+      response = @langchain_client.ask(question: params[:value])
       @raw_response = response.raw_response
     end
+  end
+
+private
+  def init_langchain_client
+    llm = Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"])
+    @langchain_client = Langchain::Vectorsearch::Pgvector.new(index_name: "Documents", url: "postgresql://langchain_user:langchain_pwd@localhost/test_langchain_development", llm: llm)
   end
 end
