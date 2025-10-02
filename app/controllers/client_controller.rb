31c58ca87d89b39367dfe779c00118506b75dd20
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2025-10-02 16:31:53 +0200

    some small code optim

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,10 +1,10 @@
 class ClientController < ApplicationController
   def ask
     if params[:value].present?
-      client = LangchainClient.instance
-      response = client.vector_search.ask(question: params[:value])
-      @raw_response = response.raw_response
-      @message = @raw_response["choices"][0]["message"]["content"]
+      response = LangchainClient.instance.vector_search.ask(question: params[:value])
+      raw_response = response.raw_response
+      @message = raw_response["choices"][0]["message"]["content"]
+      @json_response = JSON.pretty_generate(raw_response)
     end
   end
 end
